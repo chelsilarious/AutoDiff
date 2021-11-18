1f69d555b6b979ca2e56a7b53c5e53e988fe4ff8
Chelsea Z. Chen <81481661+chelsilarious@users.noreply.github.com>
2021-11-18 18:19:11 -0500

    Added __init__ file for package

---
+++ src/cs107_package/__init__.py
@@ -0,0 +1,6 @@
+from .forwardNode import ForwardNode
+from .reverseNode import ReverseNode
+from .utils import constant, sin, cos, log, exp, sqrt, tan, arctan, arcsin, arccos, tanh, sinh, cosh, log_base, cot, sec
+
+__all__ = ['ForwardNode', 'ReverseNode', 'constant', 'sin', 'cos', 'log', 'exp', 'sqrt', 'tan', 
+           'arctan', 'arcsin', 'arccos', 'tanh', 'sinh', 'cosh', 'log_base', 'cot', 'sec']
