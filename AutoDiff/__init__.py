a1f0dc9981817d3b42844ae3ef72f38c192f4f9f
chelsilarious <chelsea991205@outlook.com>
2021-12-09 12:22:54 -0500

    Updated code

--- AutoDiff/__init__.py
+++ AutoDiff/__init__.py
@@ -3,6 +3,9 @@ import numpy as np
 from .forwardNode import ForwardNode
 from .reverseNode import ReverseNode
 from .utils import *
+from .__main__ import *
 
 __all__ = ['ForwardNode', 'ReverseNode', 'sin', 'cos', 'log', 'exp', 'sqrt', 'tan',
-           'arctan', 'arcsin', 'arccos', 'tanh', 'sinh', 'cosh', 'log_base', 'cot', 'sec', 'csc']
+           'arctan', 'arcsin', 'arccos', 'tanh', 'sinh', 'cosh', 'log_base', 'cot', 'sec', 'csc',
+           'init_trace', 'create_node', 'gradientF', 'gradientR',
+           'forward_auto_diff', 'reverse_auto_diff', 'auto_diff']
